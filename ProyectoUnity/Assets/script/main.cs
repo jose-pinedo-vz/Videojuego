0a290615c9bc22f93b05d5b3029ae7fb5748abba
Jose Pinedo <josepinedovaldes@gmail.com>
2026-09-12 11:28:05 -0600

    Modificacion imporntete a las interfaz de usuario. Se modifico la distribucion de estas, antes se ocultaban y mostraban los botones a convenciancia, ya lo dividi en paneles, y haora solo botones solo son hijos de los paneles, es mas facil controlar un panel que 6 botones

--- ProyectoUnity/Assets/script/main.cs
+++ ProyectoUnity/Assets/script/main.cs
@@ -1,33 +1,54 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-
 using Unity.Services.Core;
 using Unity.Services.Authentication;
 
 public class inicio : MonoBehaviour
 {
+    [Header("Paneles de interfaz")]
+    [SerializeField] private GameObject PanelInicio;
+    [SerializeField] private GameObject PanelSeleccion;
+    [SerializeField] private GameObject PanelHost;
+    [SerializeField] private GameObject PanelCliente;
+
+
     // declaracionde componentes
     public GameObject Mapa3D;
     public GameObject piso;
-
     public TextMeshProUGUI txt_inicio;
-
+    public TextMeshProUGUI txt_codigo;
+    public TextMeshProUGUI txt_info;
     public Button btn_iniciar;
     public Button btn_crearSala; 
     public Button btn_UnirmeSale;
     public Button btn_salir;
     public Button btn_playGame;
     public Button btn_unirmeSala;
-
     public TMP_InputField inputCode;
 
     public NetWork net;
 
-    public void UnirNombres()
+    public void ocultarPaneles()
     {
+        Mapa3D.SetActive(false);
+        piso.SetActive(false);
+        
+        // Ocultamos todos los paneles secundarios primero
+        PanelSeleccion.SetActive(false);
+        PanelHost.SetActive(false);
+        PanelCliente.SetActive(false);
+
+        // Mantenemos únicamente el panel principal encendido
+        PanelInicio.SetActive(true);
+        
+        txt_inicio.text = "Bienvenido";
         
     }
+    // public void UnirNombres()
+    // {
+        
+    // }
 
     // crear host
     // se crean los botones de crear sala y unirse a una sala, se espera que el host cree la sala y este a la 
@@ -38,13 +59,8 @@ public class inicio : MonoBehaviour
         // se espera una presala para de esta manera ser capas de elegir el host, y 
         // separarlos de los invitados 
 
-        btn_crearSala.gameObject.SetActive(true);
-        btn_UnirmeSale.gameObject.SetActive(true);
-
-        btn_iniciar.gameObject.SetActive(false);
-        btn_salir.gameObject.SetActive(false);
-        txt_inicio.gameObject.SetActive(false);
-
+        PanelSeleccion.SetActive(true);
+        PanelInicio.SetActive(false);
     }
 
 
@@ -60,27 +76,25 @@ public class inicio : MonoBehaviour
     public async void crearSala()
     {
         // creacion de la sala y el host
-        btn_UnirmeSale.gameObject.SetActive(false);
-        txt_inicio.gameObject.SetActive(true);
-        btn_playGame.gameObject.SetActive(true);
-        txt_inicio.text = "Generando codigo ...";
+
+        PanelSeleccion.SetActive(false);
+        PanelHost.SetActive(true);
+
+        txt_codigo.text = "Generando codigo ...";
         
         NetWork.iniciarHost();
         string code=await net.iniciarRelay();
-        //Debug.Log($"se crea el codigo {code}");
-        txt_inicio.text = "Code: " + code;
-
+        txt_codigo.text = "Code: " + code;
     }
 
     //  codigo y botones para unirse a la sala del host
     // se espera que el host ya haya creado la sala y este a la espera de invitados
     public async void unirmeSala()
     {
-        btn_crearSala.gameObject.SetActive(false);
-        btn_UnirmeSale.gameObject.SetActive(false);
-        txt_inicio.gameObject.SetActive(true);
-        btn_unirmeSala.gameObject.SetActive(true);
-        txt_inicio.text="Igrese el codigo de la sala";
+        PanelSeleccion.SetActive(false);
+        PanelCliente.SetActive(true);
+
+        txt_info.text="Igrese el codigo de la sala";
 
         inputCode.gameObject.SetActive(true);
 
@@ -96,12 +110,12 @@ public class inicio : MonoBehaviour
             {
                 Debug.Log($"Se unira a la sala {codigo}");
                 bool exito=await net.unirseAlHost(codigo);
-                if (exito)  txt_inicio.text="Conectado con exito";
-                else txt_inicio.text="Hubo un error";
+                if (exito)  txt_info.text="Conectado con exito";
+                else txt_info.text="Hubo un error";
             }
             else
             {
-                txt_inicio.text="Codigo incorrecto o hubo un error";
+                txt_info.text="Codigo incorrecto o hubo un error";
             }
         }
         catch (System.Exception ex)
@@ -119,49 +133,18 @@ public class inicio : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    // cargar componentes a la interfaz de usuario cuando se abre el juego 
-    public void cargarComponentes()
-    {
-        Mapa3D.SetActive(false);
-        piso.SetActive(false);
-
-        // botones sueltos y para usarse despues
-        btn_crearSala.gameObject.SetActive(false);
-        btn_UnirmeSale.gameObject.SetActive(false);
-        btn_playGame.gameObject.SetActive(false);
-        inputCode.gameObject.SetActive(false);
-        btn_unirmeSala.gameObject.SetActive(false);
-
-    }
 
     private void Start()
     {
-        // esta funcion es el inicio de todo 
-        // su objetico, o por lo menos en este momento sera el crear las vistas e interfaces principales
-        
         iniciarServicios();
-        txt_inicio.text = "Bienvenido";
-        cargarComponentes();
+        ocultarPaneles();
+        
 
-        if (btn_iniciar != null)
-        {
-            btn_iniciar.onClick.AddListener(iniciar);
-        }
-        if (btn_salir != null)
-        {
-            btn_salir.onClick.AddListener(salir);
-        } 
-        if (btn_crearSala != null)
-        {
-            btn_crearSala.onClick.AddListener(crearSala);
-        }
-        if (btn_crearSala != null)
-        {
-            btn_UnirmeSale.onClick.AddListener(unirmeSala);
-        }
-        if (btn_unirmeSala!=null)
-        {
-            btn_unirmeSala.onClick.AddListener(juntarPlayer);
-        }
+        // Asignación corregida de listeners
+        if (btn_iniciar != null) btn_iniciar.onClick.AddListener(iniciar);
+        if (btn_salir != null) btn_salir.onClick.AddListener(salir);
+        if (btn_crearSala != null) btn_crearSala.onClick.AddListener(crearSala);
+        if (btn_UnirmeSale != null) btn_UnirmeSale.onClick.AddListener(unirmeSala); // Corregido
+        if (btn_unirmeSala != null) btn_unirmeSala.onClick.AddListener(juntarPlayer);
     }
 }
